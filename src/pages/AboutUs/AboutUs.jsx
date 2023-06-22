af8ba3a16fc185dc06059ac64344e1d0ba96a5b1
ewagorka <ewagorka99@gmail.com>
2023-06-22 21:10:30 +0100

    updated About Us

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import './AboutUs.css'
 
-import heroImage from "../../images/heroImage.png"
+import laszloProfile from "../../images/laszloProfile.jpg"
+import ewaProfile from "../../images/ewaProfile.jpeg"
+
+const laszloGithub ="";
+const laszloLinkedin ="";
 
 const AboutUs = () => {
   return (
@@ -15,17 +19,27 @@ const AboutUs = () => {
         </div>
       </div>
       <div className="container">
-        <div className="row">
+        <div className="row col-md-12">
           
             <div className="col-md-4 team-member">
-            <img src={heroImage} alt="Woman listening to music on headphones" className="img-fluid"/>
+            <img src={laszloProfile} alt="Woman listening to music on headphones" className="img-fluid"/>
               <h3>Laszlo</h3>
+              <p>Laszlo is super cool!</p>
+              <div className="contact-buttons">
+              <a href={laszloGithub} className="CTA" role="button" aria-pressed="true">Go to lyrics</a>
+              <a href={laszloLinkedin} className="CTA" role="button" aria-pressed="true">Go to lyrics</a>
+              </div>
             </div>
             <div className="col-md-1">
             </div>
             <div className="col-md-4 team-member">
-            <img src={heroImage} alt="Woman listening to music on headphones" className="img-fluid"/>
+            <img src={ewaProfile} alt="Woman listening to music on headphones" className="img-fluid"/>
               <h3>Ewa</h3>
+              <p>Ewa is super cool!</p>
+              <div className="contact-buttons">
+              <a href={laszloGithub} className="CTA" role="button" aria-pressed="true">Go to lyrics</a>
+              <a href={laszloLinkedin} className="CTA" role="button" aria-pressed="true">Go to lyrics</a>
+              </div>
             </div>
           
         </div>
